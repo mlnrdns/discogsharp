1fcf8e16b70f30b3b8e310df077d99b562696f84
molnardenes <molnardenes@pm.me>
2022-02-20 00:55:03 +0100

    feat: GetCollectionItemsByReleaseAsync

--- src/discogsharp/Services/ICollectionService.cs
+++ src/discogsharp/Services/ICollectionService.cs
@@ -1,4 +1,5 @@
 using discogsharp.Domain;
+using discogsharp.Utils;
 
 namespace discogsharp.Services
 {
@@ -8,6 +9,12 @@ namespace discogsharp.Services
         Task<AddFolderResponse> AddFolderAsync(string username, string folder, CancellationToken cancellationToken = default);
         Task<Folder> GetFolderAsync(string username, int id, CancellationToken cancellationToken = default);
         Task<AddFolderResponse> RenameFolderAsync(string username, int id, string newName, CancellationToken cancellationToken = default);
-        Task<Folder> DeleteFolderAsync(string username, int id, CancellationToken cancellationToken = default);
+        Task<Folder> DeleteFolderAsync(string username, int id, CancellationToken cancellationToken = default);        
+        Task<PaginatedResponse<CollectionItem>> GetCollectionItemsByReleaseAsync(
+                string username,
+                int releaseId,
+                int page = Constants.DefaultPage,
+                int perPage = Constants.DefaultPerPage,
+                CancellationToken cancellationToken = default);
     }
 }
